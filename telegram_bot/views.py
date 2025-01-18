50c162bdebd2acb3a5404594ac449ffb5064389a
Denny Baldini <dennybaldini@gmail.com>
2025-01-18 20:28:06 +0100

    works

--- telegram_bot/views.py
+++ telegram_bot/views.py
@@ -6,10 +6,6 @@ from telegram import Bot
 
 @csrf_exempt
 def start(request):
-    with open("called", "w") as f:
-        f.write("called")
-
     # bot = Bot(settings.TELEGRAM_BOT_TOKEN)
     # bot.sendMessage(chat_id=chat_id)
-
     return JsonResponse({"foo": "bar"})
